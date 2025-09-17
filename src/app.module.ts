c4d4aba3fec4907929294a5784257e443a6cc619
vuxxs <57200277+vuxxs@users.noreply.github.com>
2025-09-17 15:08:46 +0300

    Add UniswapV2Module import to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GasPriceModule } from './gas-price/gas-price.module';
+import { UniswapV2Module } from './uniswap-v2/uniswap-v2.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), GasPriceModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GasPriceModule,
+    UniswapV2Module,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
