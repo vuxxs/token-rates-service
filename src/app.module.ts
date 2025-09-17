5ecd7f2b22523035548047f0efb1b69ec8889e2b
vuxxs <57200277+vuxxs@users.noreply.github.com>
2025-09-17 14:25:42 +0300

    Add ConfigModule and GasPriceModule imports to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GasPriceModule } from './gas-price/gas-price.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), GasPriceModule],
   controllers: [AppController],
   providers: [AppService],
 })
